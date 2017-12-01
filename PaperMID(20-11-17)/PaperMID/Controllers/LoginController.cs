2c72c4d5560e9b5313d31d06eb700f21698c5ba5
juicab <chuchoyh23@gmail.com>
2017-12-01 08:24:01 -0600

    Cambio de FrontEnd
    cambio de front cliente y publico

---
+++ PaperMID(20-11-17)/PaperMID/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaperMID.Models;
+using PaperMID.BO;
+
+namespace PaperMID.Controllers
+{
+    public class LoginController : Controller
+    {
+        LoginModel oLoginModel;
+        DireccionModel oDireccionModel = new DireccionModel();
+        BO.LoginBO oLoginBO;
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ PaperMID(20-11-17)/PaperMID/Style/Front/assets/css/Personalizacion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaperMID.Style.Front.assets.css
+{
+    public class Personalizacion
+    {
+    }
+}
